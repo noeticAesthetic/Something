c615cabe203325d46d4d1a71cb36e9f2d586ea9b
noeticAesthetic <clintecurtis@gmail.com>
2014-09-14 21:27:16 -0500

    Initial commit of alpha version 0.4.

---
+++ Something/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Something.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Home Page";
+            return View();
+        }
+
+        public ActionResult Credits()
+        {
+            ViewBag.Message = "Site made possible by:";
+            return View();
+        }
+
+        public ActionResult ContactForm()
+        {
+            ViewBag.Message = "Contact us.";
+            return View();
+        }
+
+        public ActionResult Recommended()
+        {
+            ViewBag.Message = "Recommened.";
+            return View();
+        }
+
+        public ActionResult TODO()
+        {
+            ViewBag.Message = "TODO List.";
+            return View();
+        }
+    }
+}
